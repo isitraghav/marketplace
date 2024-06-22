945034d38122edfa6fa31da40f0bf31a7db654fd
Raghav Yadav <isitraghav@gmail.com>
2024-06-22 16:40:04 +0530

    clean boilerplate

---
+++ src/pages/profile.jsx
@@ -0,0 +1,10 @@
+import { Block, Navbar, Page } from "framework7-react";
+
+export default function HomePage() {
+  <Page name="home">
+    <Navbar title="Home" />
+    <Block>
+      <p>Home tab</p>
+    </Block>
+  </Page>
+}
